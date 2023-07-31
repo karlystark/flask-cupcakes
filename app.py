734c58243e7320378e5772555d55bbb4286b961a
Karly Stark <karlystark@gmail.com>
2023-07-31 12:29:56 -0700

    added Cupcake model

--- app.py
+++ app.py
@@ -1,1 +1,28 @@
 """Flask app for Cupcakes"""
+
+import os
+
+from flask import Flask, request, jsonify
+from flask_debugtoolbar import DebugToolbarExtension
+
+from models import connect_db, db, Cupcake
+
+app = Flask(__name__)
+
+app.config['SECRET_KEY'] = "secret"
+
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get(
+    "DATABASE_URL", "postgresql:///cupcakes")
+
+connect_db(app)
+
+# Having the Debug Toolbar show redirects explicitly is often useful;
+# however, if you want to turn it off, you can uncomment this line:
+#
+app.config['DEBUG_TB_INTERCEPT_REDIRECTS'] = False
+
+toolbar = DebugToolbarExtension(app)
+
+
+
+
--- models.py
+++ models.py
@@ -1,1 +1,50 @@
 """Models for Cupcake app."""
+
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
+
+DEFAULT_IMAGE_URL = "https://tinyurl.com/demo-cupcake"
+
+def connect_db(app):
+    """Connect this database to provided Flask app.
+
+    You should call this in your Flask app.
+    """
+
+    app.app_context().push()
+    db.app = app
+    db.init_app(app)
+
+
+class Cupcake(db.Model):
+    """Cupcake"""
+
+    __tablename__ = "cupcakes"
+
+    id = db.Column(
+        db.Integer,
+        primary_key=True,
+        autoincrement=True
+    )
+
+    flavor = db.Column(
+        db.String(50),
+        nullable=False
+    )
+
+    size = db.Column(
+        db.String(15),
+        nullable=False
+    )
+
+    rating = db.Column(
+        db.Integer,
+        nullable=False
+    )
+
+    image_url = db.Column(
+        db.String(500),
+        nullable=False,
+        default=DEFAULT_IMAGE_URL
+    )
